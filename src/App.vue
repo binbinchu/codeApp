c5dfe74ad6643b650d67f2be16328a6d762a4d62
chubinbin <chubinbin@tyb365.com>
2018-12-18 12:25:34 +0800

    首页

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,40 @@
 <script>
-export default {
-  created () {
-    // 调用API从本地缓存中获取数据
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+  export default {
+    created () {
+      // 调用API从本地缓存中获取数据
+      const logs = wx.getStorageSync('logs') || []
+      logs.unshift(Date.now())
+      wx.setStorageSync('logs', logs)
 
-    // eslint-disable-next-line
-    console.log('app created and cache logs by setStorageSync')
+      // eslint-disable-next-line
+      console.log('app created and cache logs by setStorageSync')
+    }
   }
-}
 </script>
 
-<style>
-.container {
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
-  padding: 200rpx 0;
-  box-sizing: border-box;
-}
-/* this rule will be remove */
-* {
-  transition: width 2s;
-  -moz-transition: width 2s;
-  -webkit-transition: width 2s;
-  -o-transition: width 2s;
-}
+<style lang="scss">
+  @import "./_sass/reset";
+
+  page {
+    height: 100%;
+    background: #f1f1f1;
+  }
+
+  .container {
+    width: rpx(750);
+    box-sizing: border-box;
+  }
+
+  /* this rule will be remove */
+  * {
+    transition: width 2s;
+    -moz-transition: width 2s;
+    -webkit-transition: width 2s;
+    -o-transition: width 2s;
+  }
+
+  .section {
+    margin-top: rpx(20);
+    display: block;
+  }
 </style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,35 @@
 <template>
   <div class="container" @click="clickHandle('test click', $event)">
-    <i-button type="success">成功</i-button>
-    <i-icon type="activity" size="28" color="#80848f" />
+    <div class="search">
+      <i-input type="number" left title="搜索" mode="wrapped" i-class="input_us"/>
+    </div>
+    <div class="section swiper">
+      <swiper
+        :autoplay="autoplay"
+        :indicator-dots="indicatorDots"
+        :interval="interval"
+        :duration="duration"
+      >
+        <block v-for="(item,index) in imgUrls" :key="index">
+          <swiper-item>
+            <image :src="item" class="slide-image"/>
+          </swiper-item>
+        </block>
+      </swiper>
+    </div>
+    <div class="section tags">
+      公告：这是一条测试公告
+    </div>
+    <div class="section shopping-div quick-buy">
+      <div class="shopping-more quick-buy-more">
+        <div class="more">更多</div>
+      </div>
+      <div class="shopping-list-image">
+        <div class="shopping-image-box">
+          <img src="" alt="">
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,7 +38,16 @@
     data () {
       return {
         motto: 'Hello World',
-        userInfo: {}
+        userInfo: {},
+        imgUrls: [
+          'http://pic.58pic.com/58pic/14/21/42/32U58PICj6I_1024.jpg',
+          'http://pic2.ooopic.com/12/62/89/46bOOOPICa6_1024.jpg',
+          'http://pic12.photophoto.cn/20090715/0020033073935424_b.jpg'
+        ],
+        indicatorDots: false,
+        autoplay: true,
+        interval: 5000,
+        duration: 1000
       }
     },
 
@@ -47,5 +84,59 @@
 </script>
 
 <style scoped lang="scss">
+  @import "../../src/_sass/reset";
 
+  .search {
+    width: rpx(750);
+    height: rpx(92);
+  }
+
+  swiper {
+    width: 100%;
+    height: rpx(425);
+    image {
+      width: 100%;
+      height: 100%;
+    }
+  }
+
+  .tags {
+    background: #ffffff;
+    height: rpx(68);
+    padding-left: rpx(20);
+    padding-right: rpx(20);
+    line-height: rpx(68);
+    color: #959595;
+    font-size: rpx(24);
+  }
+
+  .shopping-div {
+    padding-left: rpx(20);
+    padding-right: rpx(20);
+    background: #ffffff;
+    .shopping-more {
+      width: 100%;
+      display: flex;
+      flex-direction: row-reverse;
+      justify-content: space-between;
+      align-items: center;
+      align-content: center;
+      .more {
+        color: #949494;
+        fon-size: rpx(24);
+      }
+    }
+    .shopping-list-image {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      align-items: center;
+      align-content: center;
+      .shopping-image-box {
+        width: rpx(236);
+        height: rpx(236);
+        background: rgba(0, 0, 0, 0.5);
+      }
+    }
+  }
 </style>
