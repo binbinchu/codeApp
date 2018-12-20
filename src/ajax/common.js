ba095915eb235a9e5097392799909972982a8570
chubinbin <chubinbin@tyb365.com>
2018-12-20 18:29:36 +0800

    打包bug修改

--- src/ajax/common.js
+++ src/ajax/common.js
@@ -10,11 +10,11 @@ export default {
     return _fly.post(`Goods.getSearchName`, data, config)
   },
   // 获取分类
-  getGoodsClassify(data,config){
-    return _fly.post(`Goods.getClassify`,data,config)
+  getGoodsClassify (data, config) {
+    return _fly.post(`Goods.getClassify`, data, config)
   },
   // 获取全部pid!=0的分类
-  getAllClassify(data,config){
-    return _fly.post(`getHomeClassify`,data,config)
+  getAllClassify (data, config) {
+    return _fly.post(`getHomeClassify`, data, config)
   }
 }
--- src/pages/counter.vue
+++ src/pages/counter.vue
@@ -20,7 +20,7 @@
               <i-grid i-class="item-border">
                 <i-grid-item i-class="item-border">
                   <i-grid-icon>
-                    <image :src="item.img" />
+                    <image :src="item.img"/>
                   </i-grid-icon>
                   <i-grid-label i-class="item-font">{{item.name}}</i-grid-label>
                 </i-grid-item>
@@ -34,110 +34,106 @@
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      // init
-      active: 0,
-      classifiesData: [],
-      classify0: [
-        {
-          id: 0,
-          img: "",
-          name: "全部"
-        }
-      ],
-      childClassifies: []
-    };
-  },
-  /*
-  生命周期开始
-  */
-  created() {
-    this.getClassifiesEqZero();
-    // 获取全部pid != 0 的
-    this.getAllClassifiesNeqZero();
-  },
-
-  methods: {
-    // init event
-    async getClassifiesEqZero() {
-      try {
-        let data = await this.getClassifuies(0);
-        console.log("data: ", data);
-        this.classify0 = this.classify0.concat(data);
-      } catch (e) {
-        console.log(e);
+  export default {
+    data () {
+      return {
+        // init
+        active: 0,
+        classifiesData: [],
+        classify0: [
+          {
+            id: 0,
+            img: '',
+            name: '全部'
+          }
+        ],
+        childClassifies: []
       }
     },
-    // click event
-    async getChildClassifies(id) {
-      this.active = id;
+    /*
+    生命周期开始
+    */
+    created () {
+      this.getClassifiesEqZero()
+      // 获取全部pid != 0 的
+      this.getAllClassifiesNeqZero()
+    },
 
-      try {
-        if(id != 0){
-          let data = await this.getClassifuies(id);
-          console.log("data: ", data);
-          this.childClassifies = data;
-        }else{
-          this.getAllClassifiesNeqZero();
+    methods: {
+      // init event
+      async getClassifiesEqZero () {
+        try {
+          let data = await this.getClassifuies(0)
+          console.log('data: ', data)
+          this.classify0 = this.classify0.concat(data)
+        } catch (e) {
+          console.log(e)
         }
-      } catch (e) {
-        console.log(e);
-      }
-    },
-    // get data
-    getClassifuies(pid = 0) {
-      return new Promise((resolve, reject) => {
-        this.$ajax
-          .getGoodsClassify({ pid })
-          .then(({ data }) => {
-            resolve(data);
-          })
-          .catch(e => {
-            reject(e);
-          });
-      });
-    },
-    getAllClassifiesNeqZero() {
-      new Promise((resolve, reject) => {
-        this.$ajax
-          .getAllClassify({ num:99 })
-          .then(({ data }) => {
-            // console.log(data)
+      },
+      // click event
+      async getChildClassifies (id) {
+        this.active = id
+
+        try {
+          if (id !== 0) {
+            let data = await this.getClassifuies(id)
+            console.log('data: ', data)
             this.childClassifies = data
-          })
-          .catch(e => {
-            reject(e);
-          });
-      });
+          } else {
+            this.getAllClassifiesNeqZero()
+          }
+        } catch (e) {
+          console.log(e)
+        }
+      },
+      // get data
+      getClassifuies () {
+        const obj = {
+          pid: 0
+        }
+        this.$ajax.getGoodsClassify(obj).then((res) => {
+          console.log(res)
+        })
+      },
+      getAllClassifiesNeqZero () {
+        const obj = {
+          num: 99
+        }
+        this.$ajax.getAllClassify(obj).then(({ data }) => {
+          this.childClassifies = data
+        })
+      }
     }
   }
-};
 </script>
 <style>
-/* @import "../../src/_sass/reset"; */
-.counter-warp {
-  background-color: #ffffff !important;
-  text-align: center;
-  /* margin-top: 10px; */
-}
-.left-list{
-  /* background-color: #f1f1f1 !important; */
-  height: 100%;
-}
-.item-border {
-  border: none !important;
-}
-.item-font {
-  font-size: 30rpx !important;
-}
-.no-active {
-  background-color: #f1f1f1 !important;
-}
-.active {
-  color: red;
-  background-color: #ffffff !important;
-  font-weight: bold;
-}
+  /* @import "../../src/_sass/reset"; */
+  .counter-warp {
+    background-color: #ffffff !important;
+    text-align: center;
+    /* margin-top: 10px; */
+  }
+
+  .left-list {
+    /* background-color: #f1f1f1 !important; */
+    height: 100%;
+  }
+
+  .item-border {
+    border: none !important;
+  }
+
+  .item-font {
+    font-size: 30 rpx !important;
+  }
+
+  .no-active {
+    background-color: #f1f1f1 !important;
+  }
+
+  .active {
+    color: red;
+    background-color: #ffffff !important;
+    font-weight: bold;
+  }
 </style>
